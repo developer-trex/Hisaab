f84c4c5aea4a89f93db8e682fd8c1d43b04b0da2
trex-developers <trex.developers@gmail.com>
2019-09-08 12:04:56 +0530

    New Project

--- app/src/main/java/com/application/hisaab/MainActivity.java
+++ app/src/main/java/com/application/hisaab/MainActivity.java
@@ -1,14 +1,76 @@
 package com.application.hisaab;
 
+import android.app.KeyguardManager;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOCK_REQUEST_CODE = 221;
+    private static final int SECURITY_SETTING_REQUEST_CODE = 233;
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authenticateApp();
     }
+
+    private void authenticateApp() {
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Intent i = keyguardManager.createConfirmDeviceCredentialIntent(getResources().getString(R.string.unlock), getResources().getString(R.string.confirm_pattern));
+            try {
+                startActivityForResult(i, LOCK_REQUEST_CODE);
+            } catch (Exception e) {
+
+                Intent intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
+                try {
+                    startActivityForResult(intent, SECURITY_SETTING_REQUEST_CODE);
+                } catch (Exception ex) {
+
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+
+            case SECURITY_SETTING_REQUEST_CODE:
+
+                if (isDeviceSecure()) {
+                    Toast.makeText(this, getResources().getString(R.string.device_is_secure), Toast.LENGTH_SHORT).show();
+                    authenticateApp();
+                } else {
+                    textView.setText(getResources().getString(R.string.security_device_cancelled));
+                }
+
+                break;
+        }
+    }
+
+    /**
+     * method to return whether device has screen lock enabled or not
+     **/
+    private boolean isDeviceSecure() {
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && keyguardManager.isKeyguardSecure();
+
+
+    }
+
+
 }
